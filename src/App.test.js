76741e9eb8762bd6e04958d4daeaaa2d32c30d52
unknown <ngengwalutteur11@gmail.com>
2020-05-08 14:34:29 -0700

    First React With JSX

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import React_With_JSX from './components/React_With_JSX';
+
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
---
+++ src/components/React_With_JSX.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const React_With_JSX = (props) => {
+    
+    return(
+        <div>
+            
+            <h1>Hello Dojo</h1> <h2>This is the thinks I can do</h2>
+            <ul>
+                <li>Learn react</li>
+                <li>Clemb Mt Everest</li>
+                <li>Play Music</li>
+                <li>Run Marathon</li>
+            </ul>
+        </div>
+    )
+
+}
+export default React_With_JSX;
